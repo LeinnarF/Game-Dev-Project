ab6110757ed8a35e04c21261dc2fc3c749d0a1e3
Watashiii5 <jm.pasamba5@gmail.com>
2025-04-07 23:06:32 +0800

    MOBMENT 4Directions -Added New Fox Prefab -Spawner Prefab

--- Assets/MOBMENT.cs
+++ Assets/MOBMENT.cs
@@ -10,18 +10,46 @@ public class WalkingMOB : MonoBehaviour
     float direction = 1;
     public float moveSpeed = 3;
     Rigidbody2D rb;
+    private Camera mainCamera;
+    private bool isDedInvoked = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        mainCamera = Camera.main;
         rb = GetComponent<Rigidbody2D>();
         InvokeRepeating(nameof(flipDirection), timer, timer);
-        InvokeRepeating(nameof(ded), dedTimer, dedTimer);
+        InvokeRepeating(nameof(movement), timer, timer);
+        
     }
     
     // Update is called once per frame
     void Update()
     {  
-        rb.linearVelocity = new (direction * moveSpeed, rb.linearVelocity.y);
+       
+        
+        if (IsOutsideCamera())
+        {
+        isDedInvoked = true;
+        InvokeRepeating(nameof(ded), dedTimer, dedTimer);
+        }else if (IsInsideCamera() && isDedInvoked)
+        {
+            CancelInvoke(nameof(ded));
+            isDedInvoked = false;
+        }
+    }
+
+    void movement()
+    {
+         rb.linearVelocity = new ( rb.linearVelocity.x, rb.linearVelocity.y);
+        int selection = Random.Range(0, 2);
+        if (selection == 0)
+        {
+            rb.linearVelocity = new ( rb.linearVelocity.x, direction * moveSpeed);
+        }else if (selection >= 1)
+        {
+            rb.linearVelocity = new (direction * moveSpeed, rb.linearVelocity.y);
+        }
     }
     void ded()
     {
@@ -33,28 +61,28 @@ public class WalkingMOB : MonoBehaviour
         direction *= -1;
         if(direction > 0){
             transform.localScale = new (Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-    
+            
         }else if(direction < 0){
         transform.localScale = new (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
         }
     }
-    // private void OnTriggerEnter2D(Collider2D collision)
-    // {
-    //     if(collision.gameObject.CompareTag("Mob Collider"))
-    //     {
-    //         flipDirection();
-    //     }
-        
-        
-    // }
-    // private void OnCollisionEnter2D(Collision2D collision)
-    // {
-    //     if(collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("edge") || collision.gameObject.CompareTag("Tree"))
-    //     {
-    //         flipDirection();
-            
-    //     }
-    // }
+
+     private bool IsOutsideCamera()     
+    {
+        // Get the object's position in viewport coordinates
+        UnityEngine.Vector3 viewportPosition = mainCamera.WorldToViewportPoint(transform.position);
+
+        // Check if the object is outside the viewport
+        return viewportPosition.x < 0 || viewportPosition.x > 1 || viewportPosition.y < 0 || viewportPosition.y > 1;
+    }
+   private bool IsInsideCamera()
+    {
+        // Get the object's position in viewport coordinates
+        UnityEngine.Vector3 viewportPosition = mainCamera.WorldToViewportPoint(transform.position);
+
+        // Check if the object is inside the viewport
+        return viewportPosition.x >= 0 && viewportPosition.x <= 1 && viewportPosition.y >= 0 && viewportPosition.y <= 1;
+    }
     
   
 }
